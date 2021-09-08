496d52049cd690eec3a0cf7b15055216b8e892e7
JefthavanEunen <57633283+JefthavanEunen@users.noreply.github.com>
2021-09-08 09:45:33 +0200

    Separation of concerns for responsive navigation.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -20,59 +20,8 @@
 
 <!-- link voor hamburger menu icoon -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<!-- style voor hamburger menu -->
-<style>
-.topnav {
-  background-color: #333;
-  overflow: hidden;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #04AA6D;
-  color: white;
-}
-
-.topnav .icon {
-  display: none;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {display: none;}
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .topnav.responsive {position: relative;}
-  .topnav.responsive a.icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-}
-</style>
+<!-- link naar style voor hamburger menu -->
+<link rel="stylesheet" type="text/css" href="{{url('css/header.css')}}">
 
 <div class="topnav" id="myTopnav">
 	<a  <?php if (end($pieces) == ""){echo 'class="activenav"';}?> href="{{url('/')}}">Profiel </a>
@@ -91,14 +40,5 @@
 </div>
 
 <!-- script voor hamburger menu -->
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-</script>
+<script src="{{url('js/header.js')}}"></script> 
 <br>
