54f13b2d73941896af571a575b61b7975da7780d
JefthavanEunen <57633283+JefthavanEunen@users.noreply.github.com>
2021-10-03 16:50:28 +0200

    More tests for the scraper.

--- resources/views/vacature-scraper.blade.php
+++ resources/views/vacature-scraper.blade.php
@@ -70,6 +70,12 @@ $item = $html->find('div[id="mosaic-provider-jobcards"]',0);
 $outside_radius = $html->find('div[id="original_radius_result"]',0); 
 ?>
 
+{{var_dump($html)}}
+{{"line...."}}
+{{var_dump($item)}}
+{{"line...."}}
+{{var_dump($outside_radius)}}
+
 <div style="margin: auto; width: 85%; height: auto;">
     <h2 style="text-align: center;">Vacatures {{$zoekopdracht}} afgelopen <?php if ($zoekperiode >1){echo $zoekperiode." dagen";}else{echo 'dag';}?></h2>
     <h5 style="text-align: center;">Deze pagina toont een aantal vacatures die gevonden zijn met een scraper op Indeed.com</h5>
@@ -131,9 +137,6 @@ $outside_radius = $html->find('div[id="original_radius_result"]',0);
 
 @else
     <br>
-    {{$item}}
-    {{"line....."}}
-    <?php if(isset($outside_item)){echo $outside_item;}?>
     <h3 style="text-align: center;">Sorry, er zijn op dit moment geen vacatures gevonden voor '<i>{{$zoekopdracht}}</i>' in de afgelopen <?php if ($zoekperiode >1){echo $zoekperiode." dagen";}else{echo 'dag';}?>.</h3><br>
     <div style="text-align: center !important; margin: auto !important;">
         <a href="{{url('vacature-scraper')}}" class="mx-auto"><button class="btn btn-primary" style="margin-bottom: 240px;">Keer terug</button></a>
